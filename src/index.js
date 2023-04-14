c1519013c89d470b08c0e288a000d458590e223b
unknown <hsuntariq@gmail.com>
2023-04-14 17:19:51 +0500

    map mrthod

--- src/index.js
+++ src/index.js
@@ -1,17 +1,90 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const books = [
+  {
+    id: 1,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81fyoFoaxlL._AC_UL254_SR254,254_.jpg",
+    title: "Dog Man: Twenty Thousand Fleas Under the Sea: A Graphic Novel",
+    price: "9.54",
+  },
+  {
+    id: 2,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/91p0c7mYcGL._AC_UL127_SR127,127_.jpg",
+    title: "Meals she eat",
+    price: "20.41",
+  },
+  {
+    id: 3,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/91p0c7mYcGL._AC_UL127_SR127,127_.jpg",
+    title: "Meals she eat",
+    price: "20.41",
+  },
+  {
+    id: 3,
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/91p0c7mYcGL._AC_UL127_SR127,127_.jpg",
+    title: "Meals she eat",
+    price: "20.41",
+  },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BookList = () => {
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          {books.map((book) => {
+            return (
+              <>
+                <div className="col-lg-3">
+                  <Book
+                    image={book.image}
+                    title={book.title}
+                    price={book.price}
+                  />
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Book = (props) => {
+  console.log(props);
+  return (
+    <>
+      <div
+        style={{
+          height: "400px",
+        }}
+        className="book d-flex justify-content-center align-items-center flex-column shadow p-3">
+        <div className="book-img">
+          <img
+            style={{
+              height: "200px",
+              width: "200px",
+              objectFit: "cover",
+            }}
+            src={props.image}
+            alt=""
+          />
+        </div>
+        <div className="book-title">
+          <h4>{props.title} </h4>
+        </div>
+        <div className="price">
+          <h6>{props.price}</h6>
+        </div>
+      </div>
+    </>
+  );
+};
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
